1eb5d377e9a81944f2c70be927b86d70532ec5f7
Quique Alvarez <quiquealvarezmonedero@gmail.com>
2017-01-10 17:44:26 +0100

    Add movement to player

---
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rigidbody2D;
+
+	void Start(){
+		rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rigidbody2D.AddForce (movement * speed);
+	}
+}
