d6aa326a77c4da97fb2d76dcb1eb2441220505ba
Quique <quiquealvarezmonedero@gmail.com>
2017-01-10 23:10:16 +0100

    Add texts

--- Assets/_Completed-Assets/Scripts/PlayerController.cs
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text winText;
 	private Rigidbody2D rigidbody2D;
+	private int count;
 
 	void Start(){
 		rigidbody2D = GetComponent<Rigidbody2D> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";	
 	}
 
 	void FixedUpdate(){
@@ -20,6 +27,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag("PickUp")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
 		}
 	}
 }
